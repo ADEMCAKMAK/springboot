8336cd2ad9de63a368cf73cd66c05d94a0f8834b
adem.cakmak <adm.cakmak67@gmail.com>
2020-04-09 13:26:02 +0300

    init spring boot project.

---
+++ src/main/java/com/example/demo/core/Document.java
@@ -0,0 +1,24 @@
+package com.example.demo.core;
+
+public class Document {
+
+    private String name;
+    private String data;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+}
