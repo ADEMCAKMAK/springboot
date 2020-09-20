4d867b8d30f4098c135276650a6d59e8b22176e0
ADEMCAKMAK <adm.cakmak67@gmail.com>
2020-09-20 21:58:56 +0300

    example of spring data jpa

--- src/main/java/com/example/demo/entity/Person.java
+++ src/main/java/com/example/demo/entity/Person.java
@@ -1,7 +1,9 @@
 package com.example.demo.entity;
 
-import org.springframework.data.annotation.Id;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 public class Person {
 
     @Id
