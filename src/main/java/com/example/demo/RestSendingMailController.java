25a69afd71a2bee42206f703836d0631beee0e4f
ademcakmak <adm.cakmak67@gmail.com>
2020-11-05 23:15:39 +0300

    fix typo

--- src/main/java/com/example/demo/RestSendingMailController.java
+++ src/main/java/com/example/demo/RestSendingMailController.java
@@ -1,20 +1,30 @@
 package com.example.demo;
 
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
 
 @RestController
 @RequestMapping(value = "/test")
 public class RestSendingMailController {
 
     @RequestMapping(value = "/sendMail")
-    public SendMailResponseDTO sendMail(@RequestBody SendMailRequestDTO requestDTO){
+    public SendMailResponseDTO sendMail(@RequestBody Object requestDTO){
 
         SendMailResponseDTO sendMailResponseDTO = new SendMailResponseDTO();
         sendMailResponseDTO.setResult("success");
-
+        System.out.println();
         return sendMailResponseDTO;
+    }
+
+    @RequestMapping(value = "request-part", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> create(@RequestPart(value = "document") Document document,
+                                    @RequestPart(value = "file") MultipartFile file) throws FileNotFoundException {
+
+        return ResponseEntity.ok(document);
     }
 }
